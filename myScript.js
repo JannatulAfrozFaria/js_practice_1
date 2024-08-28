6f656dde9c1e39160808b4bcf43ac937c32af74c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-29 01:54:16 +0600

    title addded to string number addition section

--- myScript.js
+++ myScript.js
@@ -1,6 +1,7 @@
 function myFunction(){
     document.getElementById("changePara3").innerHTML = "Demo Paragraph 3 changed."
 }
+// ADDING STRINGS AND NUMBERS------
 let x = 5 + 5;
 let y = "5" + 5;
 let z = "Hello" + 5;
