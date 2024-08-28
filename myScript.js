7300940e6e7de1200a972fcebfffbf6d560b3e6c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-29 01:53:20 +0600

    adding stringd and numbers section executed in js file

--- myScript.js
+++ myScript.js
@@ -1,3 +1,9 @@
 function myFunction(){
     document.getElementById("changePara3").innerHTML = "Demo Paragraph 3 changed."
 }
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+console.log(x);
+console.log(y);
+console.log(z);
