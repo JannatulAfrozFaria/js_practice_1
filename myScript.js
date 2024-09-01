8f851981f024c7efff8e2a0c9c4c27b66a85d46e
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-02 03:40:12 +0600

    problem 1 solve started

--- myScript.js
+++ myScript.js
@@ -195,3 +195,9 @@ console.log(newArr);
 
 //JavaScript Math Object
 console.log(Math.PI);
+
+//Print Randomly (1 to 6) for LUDU
+function getRandBetweenMinAndMax(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+
+}
