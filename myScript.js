e151b7d43a0191ee1790f74a0e2acaac6d6a7687
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-02 03:02:40 +0600

    Math.PI

--- myScript.js
+++ myScript.js
@@ -192,3 +192,6 @@ const myArr = [1, 2, 3, 4, 5,6];
 const newArr = myArr.flatMap(x => [x, x * 10]);
 console.log(newArr);
 //output: 1,10,2,20,3,30,4,40,5,50,6,60
+
+//JavaScript Math Object
+console.log(Math.PI);
