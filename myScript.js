8ed75355b40a7dc786530369a9a46182401319ae
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-03 03:55:45 +0600

    problem file seperated from practice

--- myScript.js
+++ myScript.js
@@ -195,10 +195,3 @@ console.log(newArr);
 
 //JavaScript Math Object
 console.log(Math.PI);
-
-//Print Randomly (1 to 6) for LUDU
-function getRandBetweenMinAndMax(min,max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-
-}
-console.log(getRandBetweenMinAndMax(1,6))
